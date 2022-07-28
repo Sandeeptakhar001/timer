68395a253cec864b543d2fbbbc664175cde11027
Sandeeptakhar001 <109107520+Sandeeptakhar001@users.noreply.github.com>
2022-07-28 10:28:18 +0530

    timer
    reverse timer

---
+++ timer.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 27 15:00:25 2022
+
+@author: EVOL
+"""
+
+import time
+
+
+count_time = 5
+
+for i in range(count_time+1):
+    print(i,end=">>>")
+    time.sleep(1)
+    
